43c9b4e9bb31116f82361d4b02a44077cdd050ed
Nenavath srinu Naik <45934081+nenavathsrinu@users.noreply.github.com>
2025-06-11 17:21:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,13 @@ resource "aws_instance" "jenkins_ec2" {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-south-1"
+   #dynamodb_table = "terraform-locks"    # Optional for locking
+    encrypt        = true
+  }
+}
+
