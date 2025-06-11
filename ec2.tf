7cc09baf15af2dac12483b78a5c946301e56036e
Nenavath srinu Naik <45934081+nenavathsrinu@users.noreply.github.com>
2025-06-11 11:37:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,9 @@
-echo 'provider "aws" {
+provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_instance" "jenkins_ec2" {
-  ami           = "ami-03bb6d83c60fc5f7c"
+  ami           = "ami-xxxxxxx"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "jenkins-ec2"
-    Project = "DevOps-Pipeline"
-  }
-}' > main.tf
-
-# Push to GitHub
-git add main.tf
-git commit -m "Add Terraform to launch EC2"
-git push origin main
-
+  ...
+}
