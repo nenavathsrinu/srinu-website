9f67e7daa8f9f417489a964776753c562d54fd35
srinu <nenavath2013@gmail.com>
2025-06-11 20:44:08 +0530

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   key_name      = var.key_name
-    security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = "${aws_security_group.web_sg.id}"
   tags = {
     Name = "${var.env_name}-web"
   }
