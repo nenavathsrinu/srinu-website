b26588568fe9457545c1d5f9ddab2d9731f15865
root <root@ip-172-31-7-163.ap-south-1.compute.internal>
2025-06-11 05:58:35 +0000

    tf

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+cd srinu-website  # your cloned repo directory
+
+# Create Terraform file
+echo 'provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-03bb6d83c60fc5f7c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "jenkins-ec2"
+    Project = "DevOps-Pipeline"
+  }
+}' > main.tf
+
+# Push to GitHub
+git add main.tf
+git commit -m "Add Terraform to launch EC2"
+git push origin main
+
